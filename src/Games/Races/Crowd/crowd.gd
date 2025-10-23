baf431521b764a633621938ec1ff6c4cabca3a4c
Korn666 <simonmanes.sm@gmail.com>
2025-10-23 16:41:32 -0400

    Better ground generation and crowd applauds.

--- src/Games/Races/Crowd/crowd.gd
+++ src/Games/Races/Crowd/crowd.gd
@@ -1,42 +1,49 @@
 extends Node3D
 
+class_name Crowd
+
 @export var race_manager : RaceManager
 @export var crowd : Array[Node3D]
-@onready var audio_stream : AudioStreamPlayer3D = $AudioStreamPlayer3D
+@export var audio_stream : AudioStreamPlayer3D
 @export var _start_applaud : AudioStream
 @export var _applaud_loop : AudioStream
 var _player_in: bool = false
 var _trans_anim: float = 0
 @export var trans_accel: float = 2
 @export var is_final_crowd_on_race = false
-var on_race: bool = false
+@export var on_race: bool = true
 var _player_area: Area3D
+@export var max_applaud_distance : float
 
 func _ready() -> void:		
 	audio_stream.stream = _start_applaud
 	
 func _process(delta: float) -> void:
-	if race_manager != null:
+	if race_manager:
 		on_race = race_manager._on_race
+	
+	# Too far -> Stop applauding
+	if (audio_stream.playing and _player_area and _player_area.global_position.distance_to(global_position) > max_applaud_distance):
+		_stop_clap()
+	
+	# Applaud Conditions
 	if (_trans_anim < 1 and _player_in and on_race):
 		_trans_anim = lerp(_trans_anim ,1.1, trans_accel * delta)
 		_clap(_trans_anim)
-		if (!audio_stream.playing):
+		if not audio_stream.playing:
 			_play_sound()
-	if (_trans_anim>0 and !on_race and !is_final_crowd_on_race):
+	elif (_trans_anim>0 and !on_race and !is_final_crowd_on_race):
 		_trans_anim = 0
 		_clap(_trans_anim)
 		audio_stream.stop()
-	
-	if !audio_stream.playing and _trans_anim > 0:
+	if not audio_stream.playing and _trans_anim > 0:
 		_trans_anim = 0
 		_clap(_trans_anim)
 	
-	#For humans to look at the player
+	##For humans to look at the player
 	if (_trans_anim>0):
 		for human in crowd:
 			human.look_at(_player_area.global_position, Vector3.UP, true)
-
 
 func _play_sound() -> void:
 	audio_stream.play()
@@ -52,15 +59,13 @@ func _clap(__trans_anim: float) -> void:
 	for human in crowd:
 			var anim_tree = human.get_node("AnimationTree")
 			anim_tree.set("parameters/Blend2/blend_amount", __trans_anim)
+			
+func _stop_clap()->void:
+	_player_in = false
+	_clap(0)
+	audio_stream.stop()
 
 func _on_trigger_area_entered(area: Area3D) -> void:
 	if (area.is_in_group("Player")):
 		_player_area = area
 		_player_in = true
-		
-
-func _on_trigger_area_exited(area: Area3D) -> void:
-	if (area.is_in_group("Player")):
-		_player_in = false
-		_clap(0)
-		audio_stream.stop()
--- src/Games/Races/ObstacleRace/obstacle_race_generator.gd
+++ src/Games/Races/ObstacleRace/obstacle_race_generator.gd
@@ -15,6 +15,10 @@ var default_obstacle_sample: PackedScene
 var finish_line_instance : Node3D
 @export var start_line: PackedScene
 var start_line_instance : Node3D
+@export var ground_tile: PackedScene
+var tile_length : float = 2  #the tile is a square
+var current_tiles: Array[Node3D] = []
+
 
 var race_length: int = 100
 var race_width: float = 10
@@ -28,51 +32,101 @@ var _depth_dist: float
 var _horiz_dist: float
 var _object_size: float
 
-var _start_race_pos : float = 0
-var _cursor_pos : float = 0
+var _race_start_x : float = 0
+var _current_x_pos : float = 0
 var _end_race_x_pos : float = 0
 
 func _ready() -> void:
+	_rng.randomize()
 	default_border_sample = border_sample
 	default_obstacle_sample = obstacle_sample
 	if race_data.size()>0:
 		current_race_data = race_data[0]
 		_change_current_parameters()
-	_border_generation()
-	#_border_generation(-1)
 	_obstacle_generation()
-
+	_arches_generation()
+	_border_generation()
+	_ground_generation()
+	
 func _process(delta: float) -> void:
-	if player:
+	if player and finish_line_instance:
 		if abs(player.global_position.x - finish_line_instance.global_position.x) < 1:
 			_next_level()
 
-func _border_generation(direction := 1) -> void:
-		for i in race_length:
-			var leftBorderSample: Node3D = default_border_sample.instantiate()
-			current_race_objects.append(leftBorderSample)
-			leftBorderSample.position.x = i * direction + _start_race_pos
-			leftBorderSample.position.z = - race_width/2 - leftBorderSample.scale.z
-			add_child(leftBorderSample)
-			var rightBorderSample: Node3D = default_border_sample.instantiate()
-			current_race_objects.append(rightBorderSample)
-			rightBorderSample.position.x = i * direction + _start_race_pos
-			rightBorderSample.position.z = race_width/2 + rightBorderSample.scale.z
-			add_child(rightBorderSample)
+func _next_level() -> void:
+	current_race_data_indice += 1
+	if race_data.size()<=current_race_data_indice:
+		print("fin")
+		return
+	_current_x_pos += end_offset_to_restart
+	_race_start_x = _current_x_pos
+
+	_change_current_parameters()
+	_obstacle_generation()
+	_arches_generation()
+	_border_generation()
+	_ground_generation()
+
+func _ground_generation():
+	var x_count = ceil((race_length + end_offset_to_restart) / tile_length)
+	var z_count = ceil(race_width / tile_length)
+
+	for i in range(x_count):
+		for j in range(z_count):
+			var tile: Node3D = ground_tile.instantiate()
+			tile.position = Vector3(
+				_race_start_x + i * tile_length + tile_length / 2.0,
+				0,
+				j * tile_length - race_width / 2 + tile_length / 2.0
+			)
+			add_child(tile)
+			current_tiles.append(tile)
+	
+func _arches_generation()->void:
+	if (current_race_data_indice>0):
+		var start_line_instance : Node3D = start_line.instantiate()
+		add_child(start_line_instance)
+		start_line_instance.position.x = _race_start_x
+		start_line_instance.rotate(Vector3.UP, -PI/2)
+		var left_crowd : Crowd = start_line_instance.get_child(1)
+		var right_crowd : Crowd = start_line_instance.get_child(2)
+		left_crowd.on_race = true
+		right_crowd.on_race = true
+	finish_line_instance = finish_line.instantiate()
+	add_child(finish_line_instance)
+	finish_line_instance.position.x = _current_x_pos
+	finish_line_instance.rotate(Vector3.UP, -PI/2)
+	var left_crowd : Crowd = finish_line_instance.get_child(2)
+	var right_crowd : Crowd = finish_line_instance.get_child(3)
+	left_crowd.on_race = true
+	left_crowd.visible = true
+	right_crowd.on_race = true
+	right_crowd.visible = true
+	_end_race_x_pos = finish_line_instance.position.x
+	race_length = _end_race_x_pos - _race_start_x
+
+func _border_generation() -> void:
+	for i in range(race_length):
+		var x = i + _race_start_x
+		_spawn_border(x, -race_width/2 - 1)  # left border
+		_spawn_border(x,  race_width/2 + 1)  # right border
+
+func _spawn_border(x_pos: float, z_pos: float):
+	var border = default_border_sample.instantiate()
+	current_race_objects.append(border)
+	border.position.x = x_pos
+	border.position.z = z_pos
+	add_child(border)
+
 
 func _obstacle_generation() -> void:
 	_depth_dist = _rng.randf_range(depth_dist_btw_object_range.x, depth_dist_btw_object_range.y)
-	_cursor_pos += _depth_dist
-	while _cursor_pos < race_length + _start_race_pos:
-		_obstacles_pos_and_scale(_cursor_pos, -race_width/2, race_width/2, 5)
+	_current_x_pos += _depth_dist
+	while _current_x_pos < race_length + _race_start_x:
+		_obstacles_pos_and_scale(_current_x_pos, -race_width/2, race_width/2, 5)
 		
 		_depth_dist = _rng.randf_range(depth_dist_btw_object_range.x, depth_dist_btw_object_range.y)
-		_cursor_pos += _depth_dist
-	finish_line_instance = finish_line.instantiate()
-	add_child(finish_line_instance)
-	finish_line_instance.position.x = _cursor_pos
-	finish_line_instance.rotate(Vector3.UP, -PI/2)
-	_end_race_x_pos = finish_line_instance.position.x
+		_current_x_pos += _depth_dist
 
 func _obstacles_pos_and_scale(pos_x: float, left_border_pos, right_border_pos, nb_obs)->void:
 	if (nb_obs == 0):
@@ -85,14 +139,14 @@ func _obstacles_pos_and_scale(pos_x: float, left_border_pos, right_border_pos, n
 		var size_z = _rng.randf_range(object_size_range.x, object_size_range.y)
 
 		var in_race_space = pos_z - size_z/2 > left_border_pos and pos_z + size_z/2 < right_border_pos
-		var enough_space = pos_z - size_z/2 - left_border_pos < min_passage_size or pos_z + size_z/2 - right_border_pos < min_passage_size
+		var enough_space = pos_z - size_z/2 - left_border_pos > min_passage_size or pos_z + size_z/2 - right_border_pos > min_passage_size
 
 		if in_race_space and enough_space:
 			obstacle.position.z = pos_z
 			obstacle.scale.z = size_z
 			break
 	add_child(obstacle)
-		
+
 	var dist_right = right_border_pos - (obstacle.position.z + obstacle.scale.z/2)
 	var dist_left = (obstacle.position.z - obstacle.scale.z/2) - left_border_pos
 
@@ -102,23 +156,6 @@ func _obstacles_pos_and_scale(pos_x: float, left_border_pos, right_border_pos, n
 	else:
 		if dist_left > min_passage_size + object_size_range.x:
 			_obstacles_pos_and_scale(pos_x, left_border_pos, obstacle.position.z - obstacle.scale.z/2, nb_obs - 1)
-			
-func _next_level() -> void:
-	current_race_data_indice += 1
-	if race_data.size()<=current_race_data_indice:
-		print("fin")
-		return
-	_cursor_pos += end_offset_to_restart
-	_start_race_pos = _cursor_pos
-	if (current_race_data_indice>0):
-		var start_line_instance : Node3D = start_line.instantiate()
-		add_child(start_line_instance)
-		start_line_instance.position.x = _start_race_pos
-		start_line_instance.rotate(Vector3.UP, -PI/2)
-	
-	_change_current_parameters()
-	_obstacle_generation()
-	_border_generation()
 
 func _destroy_current_race_objects()->void:
 	for i in range(current_race_objects.size() - 1, -1, -1):
@@ -137,7 +174,6 @@ func _change_current_parameters() -> void:
 		current_race_data.border_sample = default_border_sample
 	if current_race_data.obstacle_sample == null:
 		current_race_data.obstacle_sample = default_obstacle_sample
-	
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
